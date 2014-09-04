0841882cae0fb2e96e6d5cf3088e48643bfabcb5
Mehdi Rahim <mehdi@is146139.intra.cea.fr>
2014-09-04 14:47:29 +0200

    plot testing

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,6 +4,12 @@ Created on Mon Sep  1 11:09:03 2014
 
 @author: Mehdi
 """
+import numpy as np
+import pylab as pl
 
-print('Hello World')
-print('.')
+
+x=np.linspace(-10,10,10000)
+y=np.cos(x)
+
+pl.close('all')
+pl.plot(x,y)
