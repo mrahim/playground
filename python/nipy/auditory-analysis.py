3c87e773001a97cbeab424b80c358fbc06b4a26e
Mehdi Rahim <mehdi@is146139.intra.cea.fr>
2014-09-26 09:11:44 +0200

    auditory analysis

--- python/nipy/auditory-analysis.py
+++ python/nipy/auditory-analysis.py
@@ -3,48 +3,49 @@
 Auditory fMRI analysis with nipy 
 """
 
+import os
 import numpy as np
 from nipy.modalities.fmri.glm import GeneralLinearModel
 from nipy.modalities.fmri import design_matrix
 from nipy.modalities.fmri.experimental_paradigm import BlockParadigm
+from nipy.modalities.fmri.glm import FMRILinearModel
 from scipy import io
 import nibabel as nib
 import matplotlib.pylab as pl
+from nipy.labs.viz import plot_map, mni_sform, coord_transform
 
-
-
-
-# input files
-BASE_DIR_PREFIX = '/volatile/accounts/mehdi/data/MoAEpilot/fM00223/f'
-img = np.zeros((64,64,64,84))
-for i in np.arange(16,99):
-    img[...,i-16] = nib.load(BASE_DIR_PREFIX+'M00223_0'+str(i)+'.hdr').get_data()[...,0]
-
+# input image
+BASE_DIR_PREFIX = '/volatile/accounts/mehdi/data/MoAEpilot/'
+image = nib.load(BASE_DIR_PREFIX+'fM00223/4D-smoothed.nii')
 
 # design matrix
-conditions = ['rest', 'listen'] * 7
-duration = 6*ones(14)
-onset = np.arange(0,84,6)
-
+conditions = ['listen'] * 7
+duration = 6*np.ones(7)
+onset = np.arange(6,84,12)
 paradigm = BlockParadigm(con_id=conditions, onset=onset, duration=duration)
+frametimes = np.arange(6,90)
 
-M = np.zeros(84)
-for i in np.arange(6,83,12):
-    M[i:i+7]=1
-
-DM = design_matrix.make_dmtx(M,paradigm,hrf_model='Canonical')
-
-"""
-M = np.zeros(84)
-for i in np.arange(6,83,12):
-    M[i:i+7]=1
+DM = design_matrix.make_dmtx(frametimes,paradigm,hrf_model='Canonical',
+                             drift_model='blank')
 
 
-DM = design_matrix.make_dmtx(M,hrf_model='canonical',add_regs=np.ones(84))
-"""
+# GLM
 
-print DM.matrix
+GLM = FMRILinearModel(image,DM.matrix)
+GLM.fit()
 
-pl.close('all')
-pl.imshow(DM.matrix)
-pl.colorbar()
+
+# Contrast
+c = np.array([1,0])
+
+z_map, t_map, eff_map, var_map  = GLM.contrast(c,
+                 contrast_type='t',
+                 output_z=True,
+                 output_stat=True,
+                 output_effects=True,
+                 output_variance=True
+                 )
+
+for result,filename in zip([z_map, t_map, eff_map, var_map],
+                  ['z_map', 't_map', 'eff_map', 'var_map']):
+    nib.save(result,os.path.join(BASE_DIR_PREFIX,'results',filename)) 
