8a0d9c02fbb31fd0da76290c1fe8a746411609b6
Mehdi <rahim.mehdi@gmail.com>
2014-10-13 18:24:47 +0200

    a simple eprime parser

--- python/neuroimage/eprime/parse_eprime.py
+++ python/neuroimage/eprime/parse_eprime.py
@@ -2,36 +2,60 @@
 A script for parsing e-prime files
 :Author: RAHIM Mehdi
 """
-
 import os
-
-# Header Parsing Function
-def eprime_parse_header(filename):
-    ''' returns a dictionary of values in the header of the given file '''
-    header = {}
-    with open(filename,'rU') as f:
-        lines = [x.strip() for x in f.read().split('\n')]
-        for line in lines:
-            if line == "*** Header Start ***":
-                continue
-            if line == "*** Header End ***":
-                return header
-            fields = line.split(": ")
-            if len(fields) == 2:
-                header[fields[0]] = fields[1]    
+import pandas as pd
 
 # File ID Parsing Function
 def eprime_parse_filename(filename):
     ''' returns the file_id of from the filename '''
     return filename.split('-')[1]
 
-# Header Parsing
+# Quick and Dirty Parser
+def eprime_parse_data(filename):
+    ''' returns a dict of the header and a DataFrame of values
+    in the 3rd level of the given file '''
+    edf = pd.DataFrame() # Subject dataframe
+    lvl = {}    # E-Prime level 3 dict
+    hdr = {} # Header dict
+    level_flag = -1 # Flag on the current header/level
+    with open(filename, 'rU') as f:
+        lines = [x.strip() for x in f.read().split('\n')]
+        for line in lines:
+            if line in ["*** Header Start ***", "*** Header End ***"]:
+                # set the flag on header section
+                level_flag = 0
+                continue
+            if line == "*** LogFrame Start ***":
+                # reset the level 3 dict
+                lvl = {}
+                continue
+            if line == "*** LogFrame End ***":
+                # append dict according to the level
+                if lvl:
+                    edf = edf.append(lvl, ignore_index=True)
+                level_flag = -1
+                continue
+            fields = line.split(": ")
+            if fields[0] == "Level":
+                level_flag = int(fields[1])
+                continue
+            if level_flag == 3:
+                lvl[fields[0]] = ''
+                if len(fields) == 2:
+                    lvl[fields[0]] = fields[1]
+            elif level_flag in [0, 1, 2]:
+                hdr[fields[0]] = fields[1]
+    return edf, hdr
+
+
+''' Parsing all subjects and saving :
+- a session csv per subject
+- a whole subject header csv '''
+header = pd.DataFrame()
 for fn in os.listdir('eprime_files'):
     if(fn[0] == 'M'):
-        head = eprime_parse_header(os.path.join('eprime_files', fn))
-        print head['SessionDate'], '-', head['SessionTime']
-        print head['Subject'].strip(), '-', eprime_parse_filename(fn), '-', fn
-
-
-# Todo : Construct a header csv file of all the subjects
-# Todo : Construct an adequate-format file for each subject frameset
+        df, hd = eprime_parse_data(os.path.join('eprime_files', fn))
+        df.to_csv(os.path.join('eprime_files','csv',
+                               os.path.splitext(fn)[0]+'.csv'), sep=',')
+        header = header.append(hd, ignore_index=True)
+header.to_csv(os.path.join('eprime_files','csv','all_subjects.csv'), sep=',')        
---
+++ python/neuroimage/eprime/parse_eprime_header.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 13 13:47:44 2014
+
+@author: mehdi
+"""
+
+import os
+
+# Header Parsing Function
+def eprime_parse_header(filename):
+    ''' returns a dictionary of values in the header of the given file '''
+    header = {}
+    with open(filename, 'rU') as f:
+        lines = [x.strip() for x in f.read().split('\n')]
+        for line in lines:
+            if line == "*** Header Start ***":
+                continue
+            if line == "*** Header End ***":
+                return header
+            fields = line.split(": ")
+            if len(fields) == 2:
+                header[fields[0]] = fields[1]
+
+# File ID Parsing Function
+def eprime_parse_filename(filename):
+    ''' returns the file_id of from the filename '''
+    return filename.split('-')[1]
+
+# Header Parsing for all subjects
+for fn in os.listdir('eprime_files'):
+    if(fn[0] == 'M'):
+        head = eprime_parse_header(os.path.join('eprime_files', fn))
+        print head['SessionDate'], '-', head['SessionTime']
+        print head['Subject'].strip(), '-', eprime_parse_filename(fn), '-', fn
+
