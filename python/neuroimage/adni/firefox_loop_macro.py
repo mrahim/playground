8877187c0347b19ec6eb31807ced07479be2da22
Mehdi <rahim.mehdi@gmail.com>
2014-10-15 18:22:06 +0200

    pet fdg selection

---
+++ python/neuroimage/adni/firefox_loop_macro.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Generate iim file (iMacros) for Firefox
+It contains a script which fills a IDA query result of ADNI
+and save it in a collection
+@author: mehdi
+"""
+import numpy as np
+
+query_1 = 'TAB T=1\nURL GOTO=https://ida.loni.usc.edu/ladvq_results.jsp?mode=later&pageNum='
+number = '2'
+query_2 = '&project=ADNI&image_type_description=Original\nTAG POS=1 TYPE=INPUT:CHECKBOX FORM=NAME:resultForm ATTR=NAME:selectAll CONTENT=YES\nTAG POS=1 TYPE=INPUT:IMAGE ATTR=ID:add\nTAG POS=1 TYPE=SELECT FORM=NAME:addCollection ATTR=NAME:existingCollections CONTENT=%63893\nTAG POS=1 TYPE=IMG ATTR=ID:submit1'
+
+query_full = 'VERSION BUILD=8820413 RECORDER=FX\n'
+for number in np.arange(2, 73):
+    query_full += query_1+str(number)+query_2+'\n\n'
+
+print query_full
+
+text_file = open("myrequest.iim", "w+")
+text_file.write(query_full)
+text_file.close()
--- python/neuroimage/adni/select_adni_fdg_pet.py
+++ python/neuroimage/adni/select_adni_fdg_pet.py
@@ -6,20 +6,36 @@ Created on Tue Oct 14 19:25:47 2014
 """
 
 import os
-import numpy as np
 import pandas as pd
 
-# read all adni pet list
-pet_all = pd.read_csv(
-os.path.join('csv', 'pet_original_559_idaSearch_10_14_2014.csv'))
+# read all adni pet list (from IDA search : original PET of all patient)
+#filepath = os.path.join('csv', 'pet_original_559_idaSearch_10_14_2014.csv')
+filepath = os.path.join('csv', 'pet_fdg_original_all_idaSearch_10_15_2014.csv')
+pet_all = pd.read_csv(filepath)
 
+''' Deprecated as the list already contains only fdg, baseline and 1 year
 # extract FDG
-fdg_idx = ['FDG' in d for d in pet_all['Description']]
+fdg_idx = ['fdg' in d.lower() for d in pet_all['Description']]
 pet_fdg = pet_all[fdg_idx]
 
 # extract subject ID which have more than one FDG (longitudinal)
 grouped = pet_fdg.groupby('Subject_ID')
+pet_fdg_long = grouped.filter(lambda x: len(x) > 1)
+'''
 
-pet_fdg_long = grouped.filter(lambda x: len(x)>1)
+# extract baseline visits from the longitudinal fdg-pet
+baseline_idx = ['baseline' in d.lower() for d in pet_all['Visit']]
+pet_baseline = pet_all[baseline_idx]
 
-print pet_fdg_long.groupby('Visit').size()
+# extract year 1 visits from the longitudinal fdg-pet
+pet_year_1 = pd.DataFrame()
+for year_1 in ['month 12', 'year 1']:
+    year_1_idx = [year_1 in d.lower() for d in pet_all['Visit']]
+    y1 = pet_all[year_1_idx]
+    pet_year_1 = pet_year_1.append(y1, ignore_index=True)
+
+# inner join the baseline and the year 1
+pet_long = pd.merge(pet_baseline, pet_year_1, on='Subject_ID',
+                    how='inner')
+
+# get the list of subjects who have small
