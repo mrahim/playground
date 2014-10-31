3c97f72a2883772e050dbe874b216efa2127af97
Mehdi Rahim <mehdi.rahim@cea.fr>
2014-10-31 09:57:32 +0100

    psydat management

---
+++ python/neuroimage/eprime/save_to_mat.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 30 21:15:15 2014
+
+@author: Mehdi
+"""
+import numpy as np
+from scipy import io
+from collections import OrderedDict
+
+def generate_multiconditions_mat(conditions, ddurations):
+    names = np.zeros((len(conditions),), dtype=np.object)
+    onsets = np.zeros((len(conditions),), dtype=np.object)
+    durations = np.zeros((len(conditions),), dtype=np.object)
+    for i in np.arange(0, len(conditions)):
+        names[i] = conditions.keys()[i]
+        onsets[i] = conditions[names[i]]
+        durations[i] = ddurations[names[i]]
+    return names, onsets, durations
+
+
+anticip = np.random.rand(5)*100
+feedback = np.random.rand(5)*100
+click = np.random.rand(2)*100
+nada = np.random.rand(1)*100
+
+
+
+conditions = {'anticip': anticip,
+              'feedback': feedback,
+              'click': click,
+              'nada': nada}              
+
+ddurations = {'anticip': 2.,
+             'feedback': 5.,
+             'click': 0.,
+             'nada': 0.}
+
+names, onsets, durations = generate_multiconditions_mat(conditions, ddurations)
+
+io.savemat('test', {'names' : names,
+                    'onsets' : onsets,
+                    'durations' : durations})
+print names, onsets, durations 
+'''
+names
+onsets
+durations
+
+obj_arr = np.zeros((2,), dtype=np.object)
+obj_arr[0] = [1, 2]
+obj_arr[1] = 'a string'
+
+
+io.savemat('test', {'cond':obj_arr})
+
+'''
--- python/neuroimage/preprocessing/rename_files.py
+++ python/neuroimage/preprocessing/rename_files.py
@@ -1,17 +1,38 @@
 """
 A script that renames MID and T1MRI data :
 - S[xxxxx]_s005[000i].nii
+- S[xxxxx]_struct.hdr, .img
 - 
 """
 
 import os, glob
 
-BASE_DIR = os.path.join('/', 'shfj', 'Ppsypim', 'PSYDAT', 'Subjects')
+BASE_DIR = os.path.join('/', 'home', 'Ppsypim', 'PSYDAT', 'Subjects')
 
 for subject in os.listdir(BASE_DIR):
     mid_path = os.path.join(BASE_DIR, subject, 'MRI', 'MID')
     mid_files = glob.glob(os.path.join(mid_path, '[a-z|A-Z]*.nii'))
     for f in mid_files:
-        if 'Exam' in f:
-            print f
-            print f.replace('Exam','S')
+        filedir, filename = os.path.split(f)
+        filename = f.split('/')[-1]
+        if 'Exam' in filename:
+            new_filename = filename.replace('Exam','S')
+            print new_filename
+            os.chdir(filedir)
+            os.rename(filename, new_filename)
+            
+    anat_path = os.path.join(BASE_DIR, subject, 'MRI', 'T1MRI')
+    anat_files = glob.glob(os.path.join(anat_path, '*.*'))
+    for f in anat_files:
+        filedir, filename = os.path.split(f)
+        filename = f.split('/')[-1]
+        if 'nobias' in filename:
+            new_filename = filename.replace('nobias_Exam','S')
+            print new_filename
+            os.chdir(filedir)
+            os.rename(filename, new_filename)
+        elif 'Exam' in filename:
+            new_filename = filename.replace('Exam','S')
+            print new_filename
+            os.chdir(filedir)
+            os.rename(filename, new_filename)
