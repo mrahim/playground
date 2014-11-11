5d5575ae5befd021c1d00725fd82f0a3bd47e081
Mehdi Rahim <Mehdi@MacBook-Air-de-Mehdi.local>
2014-11-11 22:17:53 +0100

    rpbi

--- python/neuroimage/adni/rpbi_pet_adni.py
+++ python/neuroimage/adni/rpbi_pet_adni.py
@@ -6,3 +6,15 @@ Created on Tue Nov 11 20:18:27 2014
 """
 
 from nilearn import mass_univariate
+
+mass_univariate.randomized_parcellation_based_inference()
+
+# tested_var :
+# imaging_var 
+#mask
+#n_parcellation
+#n_parcels
+#threshold
+#n_perm
+#random_state
+#
